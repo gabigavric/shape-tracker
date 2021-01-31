f51ac1a4472a6c2ce76db4f92788fd6c680681b6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-31 12:02:03 -0800

    Complete set up of development environment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
+
 $(document).ready(function() {
   $("#triangle-checker-form").submit(function(event) {
     event.preventDefault();
@@ -7,5 +13,5 @@ $(document).ready(function() {
     const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $("#response").append("<p>" + response + "</p>");
-  })
-})
+  });
+});
