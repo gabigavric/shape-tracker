f377ea1868693c0fbf1a445d90290ed042844d2c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-02 19:20:16 -0800

    Add test for getArea()

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -3,29 +3,29 @@ import Triangle from './../src/js/triangle.js';
 describe('Triangle', () => {
   
   test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2, 4, 5);
+    const triangle = new Triangle(2,4,5);
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
 
   test('should correctly determine whether three lengths are not a triangle', () => {
-    const notTriangle = new Triangle(3, 9, 22);
+    const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
   });
 
   test('should correctly determine whether three lengths make a scalene triangle', () => {
-    const scalTriangle = new Triangle(4, 5, 7);
+    const scalTriangle = new Triangle(4,5,7);
     expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
 
   test('should correctly determine whether three lengths make an isosceles triangle', () => {
-    const isocTriangle = new Triangle(5, 5, 7);
+    const isocTriangle = new Triangle(5,5,7);
     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
   });
   
   test('should correctly determine whether three lengths make an equilateral triangle', () => {
-    const equiTriangle = new Triangle(5, 5, 5);
+    const equiTriangle = new Triangle(5,5,5);
     expect(equiTriangle.checkType()).toEqual("equilateral triangle");
   });
 });
@@ -34,7 +34,7 @@ describe('Triangle', () => {
   let reusableTriangle;
 
   beforeEach(() => {
-    reusableTriangle = new Triangle(5, 5, 5);
+    reusableTriangle = new Triangle(5,5,5);
   });
 
   test('should show how beforeEach() works', () => {
